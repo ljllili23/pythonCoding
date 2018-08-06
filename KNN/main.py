723db43e12db5c6c45242431e3aa291996f33abf
LeeJiangLee <ljllili23@gmail.com>
2018-08-06 17:16:20 +0800

    modified

--- KNN/main.py
+++ KNN/main.py
@@ -25,20 +25,22 @@ ax.set_ylabel(axLabels[1])
 ax.set_zlabel(axLabels[2])
 
 # ax.scatter(dataSet[:,0],dataSet[:,1],dataSet[:,2], s=100, c=np.array(label),alpha=.6,label=axLabels)     # 多维array 切片
-largeDosesArray = np.zero((0,3),dtype=float)
-smallDosesArray = np.zero((0,3),dtype=float)
-didntLikeArray = np.zero((0,3),dtype=float)
+largeDosesArray = np.zeros((0,3),dtype=float)
+smallDosesArray = np.zeros((0,3),dtype=float)
+didntLikeArray = np.zeros((0,3),dtype=float)
 
 # row_stack 这种append的方式效率不高，可以用给定array大小的0矩阵，然后填值的方式来代替！
 
 for item,data in zip(label,dataSet):
     if item==0:
-        largeDosesArray = np.row_stack()
+        largeDosesArray = np.row_stack((largeDosesArray,data))
     elif item==1:
-        ax.scatter(data[0], data[1], data[2], s=100, c='yellow', marker='^', alpha=.6)
+        smallDosesArray = np.row_stack((smallDosesArray,data))
     else:
-        ax.scatter(data[0], data[1], data[2], s=100, c='blue', marker='^', alpha=.6)
-
+        didntLikeArray = np.row_stack((didntLikeArray,data))
+ax.scatter(largeDosesArray[:,0],largeDosesArray[:,1],largeDosesArray[:,2],s=100,c='green',marker='o',label='largeDoses')
+ax.scatter(smallDosesArray[:,0],smallDosesArray[:,1],smallDosesArray[:,2],s=100,c='blue',marker='^',label='smallDoses')
+ax.scatter(didntLikeArray[:,0],didntLikeArray[:,1],didntLikeArray[:,2],s=100,c='red',marker='x',label='didntLike')
 ax.legend()
 plt.show()
 
